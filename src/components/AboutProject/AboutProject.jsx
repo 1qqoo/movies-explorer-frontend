e3c2ef067807c7559e01cf7098364b3a62be278f
Dmitry Karpenko <Demonkainzzzz@mail.ru>
2023-07-22 13:43:10 +0300

    добавил блок Techs

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -7,34 +7,34 @@ function AboutProject() {
         <h2 className="section-title">О проекте</h2>
         <div className="aboutProject-info">
           <article className="two-columns">
-            <h3 className="two-columns__main-text">
+            <h4 className="two-columns__main-text">
               Дипломный проект включал 5 этапов
-            </h3>
+            </h4>
             <p className="two-columns__main-paragraph">
               Составление плана, работу над бэкендом, вёрстку, добавление
               функциональности и финальные доработки.
             </p>
           </article>
           <article className="two-columns">
-            <h3 className="two-columns__main-text">
+            <h4 className="two-columns__main-text">
               На выполнение диплома ушло 5 недель
-            </h3>
+            </h4>
             <p className="two-columns__main-paragraph">
               У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
               соблюдать, чтобы успешно защититься.
             </p>
           </article>
         </div>
-        <ul className="aboutProject-list">
-          <li className="aboutProject-list__text aboutProject-list__text_colore_green">
-            1 неделя
-          </li>
-          <li className="aboutProject-list__text aboutProject-list__text_colore_grey">
-            4 недели
-          </li>
-          <li className="aboutProject-list__text">Back-end</li>
-          <li className="aboutProject-list__text">Front-end</li>
-        </ul>
+        <div className="about-project__scheme">
+          <div className="about-project__backend">
+            <span className="about-project__backend-duration">1 неделя</span>
+            <span className="about-project__scheme-title">Back-end</span>
+          </div>
+          <div className="about-project__frontend">
+            <span className="about-project__frontend-duration">4 недели</span>
+            <span className="about-project__scheme-title">Front-end</span>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -5,6 +5,36 @@ function Techs() {
     <section className="techs">
       <div className="techs__container">
         <h2 className="section-title">Технологии</h2>
+        <div className="techs-info">
+          <h3 className="techs-info__description">7 технологий</h3>
+          <p className="techs-info__paragraph">
+            На курсе веб-разработки мы освоили технологии, которые применили в
+            дипломном проекте.
+          </p>
+          <ul className="techs__spisok">
+            <li className="techs__spisok-element">
+              <p className="techs__spisok-text">HTML</p>
+            </li>
+            <li className="techs__spisok-element">
+              <p className="techs__spisok-text">CSS</p>
+            </li>
+            <li className="techs__spisok-element">
+              <p className="techs__spisok-text">JS</p>
+            </li>
+            <li className="techs__spisok-element">
+              <p className="techs__spisok-text">React</p>
+            </li>
+            <li className="techs__spisok-element">
+              <p className="techs__spisok-text">Git</p>
+            </li>
+            <li className="techs__spisok-element">
+              <p className="techs__spisok-text">Express.js</p>
+            </li>
+            <li className="techs__spisok-element">
+              <p className="techs__spisok-text">mongoDB</p>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
