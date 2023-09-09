af9d918ae5b8d069ac36db0e88e7973d20de99b2
Dmitry Karpenko <Demonkainzzzz@mail.ru>
2023-09-09 15:54:06 +0300

    refactoring overlay

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -10,70 +10,78 @@ const BurgerMenu = () => {
   };
 
   return (
-    <nav
-      className={`${
-        !isActiveBurger ? 'burger-navigation' : 'burger-navigation_type_active'
-      }`}
+    <section
+      className={`${!isActiveBurger ? 'burger' : 'burger burger_type_active'}`}
     >
-      <div className="burger-btn__container">
-        <button
-          type="button"
-          onClick={handleOpenBurger}
-          className={`${
-            !isActiveBurger ? 'burger-btn' : 'burger-btn burger-btn_type_active'
-          }`}
-        >
-          <span
+      <nav
+        className={`${
+          !isActiveBurger
+            ? 'burger__container'
+            : 'burger__container burger__container_type_active'
+        }`}
+      >
+        <div className="burger-btn__container">
+          <button
+            type="button"
+            onClick={handleOpenBurger}
             className={`${
               !isActiveBurger
-                ? 'burger-span'
-                : 'burger-span burger-span_type_active'
+                ? 'burger-btn'
+                : 'burger-btn burger-btn_type_active'
             }`}
-          ></span>
-        </button>
-      </div>
-      {isActiveBurger ? (
-        <>
-          <ul className="burger-menu__links">
-            <li>
+          >
+            <span
+              className={`${
+                !isActiveBurger
+                  ? 'burger-span'
+                  : 'burger-span burger-span_type_active'
+              }`}
+            ></span>
+          </button>
+        </div>
+        {isActiveBurger ? (
+          <>
+            <ul className="burger-menu__links">
+              <li>
+                <NavLink
+                  to="/"
+                  className="burger-menu__link"
+                >
+                  Главная
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/movies"
+                  className="burger-menu__link"
+                >
+                  Фильмы
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/saved-movies"
+                  className="burger-menu__link"
+                >
+                  Сохранённые фильмы
+                </NavLink>
+              </li>
+            </ul>
+            <div className="burger-menu__links burger-menu__links_type_account">
               <NavLink
-                to="/"
-                className="burger-menu__link"
+                to="/profile"
+                className="burger-menu__link burger-menu__link_type_account"
               >
-                Главная
+                Аккаунт
               </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/movies"
-                className="burger-menu__link"
-              >
-                Фильмы
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/saved-movies"
-                className="burger-menu__link"
-              >
-                Сохранённые фильмы
-              </NavLink>
-            </li>
-          </ul>
-          <div className="burger-menu__links burger-menu__links_type_account">
-            <NavLink
-              to="/profile"
-              className="burger-menu__link burger-menu__link_type_account"
-            >
-              Аккаунт
-            </NavLink>
-            <div className="burger-menu__link-icon"></div>
-          </div>
-        </>
-      ) : (
-        ''
-      )}
-    </nav>
+              <div className="burger-menu__link-icon"></div>
+            </div>
+          </>
+        ) : (
+          ''
+        )}
+      </nav>
+    </section>
   );
 };
 
