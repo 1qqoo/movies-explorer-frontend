bb60287f0ec42d6e834ac1ea8e51ed0e62b61562
Dmitry Karpenko <Demonkainzzzz@mail.ru>
2023-10-18 20:45:22 +0300

    useEf

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import SearchForm from '../SearchForm/SearchForm';
 import './Movies.css';
