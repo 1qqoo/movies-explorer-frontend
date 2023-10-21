8e78931c32ae35896a30a92d2c0361c26c8f72d7
Dmitry Karpenko <Demonkainzzzz@mail.ru>
2023-10-21 15:59:10 +0300

    cardsToShow

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,3 +1,4 @@
+import useResize from '../../hooks/useResize';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import './MoviesCardList.css';
 
@@ -10,19 +11,33 @@ const MoviesCardList = ({
   onDeleteSave,
   checkSavedMovies,
 }) => {
+  const screenWidth = useResize();
+
+  let cardsToShow;
+
+  if (screenWidth >= 1280) {
+    cardsToShow = 12;
+  } else if (screenWidth >= 768) {
+    cardsToShow = 8;
+  } else {
+    cardsToShow = 5;
+  }
+
   const filteredMovies = searched
-    ? movies.filter((movie) => {
-        const movieNameRU = (movie.nameRU || '').toLowerCase();
-        const movieNameEN = (movie.nameEN || '').toLowerCase();
+    ? movies
+        .filter((movie) => {
+          const movieNameRU = (movie.nameRU || '').toLowerCase();
+          const movieNameEN = (movie.nameEN || '').toLowerCase();
 
-        const query = searchQuery.search || '';
+          const query = searchQuery.search || '';
 
-        const includesSearchQuery =
-          movieNameRU.includes(query.toLowerCase()) ||
-          movieNameEN.includes(query.toLowerCase());
+          const includesSearchQuery =
+            movieNameRU.includes(query.toLowerCase()) ||
+            movieNameEN.includes(query.toLowerCase());
 
-        return includesSearchQuery && (!shortFilm || movie.duration <= 40);
-      })
+          return includesSearchQuery && (!shortFilm || movie.duration <= 40);
+        })
+        .slice(0, cardsToShow)
     : [];
 
   return (
--- src/hooks/useResize.jsx
+++ src/hooks/useResize.jsx
@@ -3,20 +3,17 @@ import { useEffect, useState } from 'react';
 function useResize() {
   const [width, setWidth] = useState(window.innerWidth);
 
-  useEffect(
-    function () {
-      function handleResize(e) {
-        setTimeout(function () {
-          setWidth(e.target.innerWidth);
-        }, 1000);
-      }
-      window.addEventListener('resize', handleResize);
-      return function () {
-        window.removeEventListener('resize', handleResize);
-      };
-    },
-    [width]
-  );
+  useEffect(() => {
+    const handleResize = (event) => {
+      setTimeout(() => {
+        setWidth(event.target.innerWidth);
+      }, 1000);
+    };
+    window.addEventListener('resize', handleResize);
+    return function () {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [width]);
 
   return width;
 }
